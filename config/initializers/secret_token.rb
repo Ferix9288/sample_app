a24ce34f043b951e389f706099855405e6d5a6ca
Felix Li <felix.li@berkeley.edu>
2012-06-30 23:44:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAmp::Application.config.secret_token = '720613bb3277e238bfa9a71674a651e4435310483d8b26ce36f16d32d0d1f49f94141702c19cce783eb4ee96bf275ba9a4383f736eaafd724ff7720758cd33de'
